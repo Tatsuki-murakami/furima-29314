bf1b88ede84252a853750b8d996e59eb71af729e
Tatsuki-murakami <69897189+Tatsuki-murakami@users.noreply.github.com>
2020-09-24 14:05:35 +0900

    テストコード記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname                        {Faker::Name.initials(number: 2)}
+    email                           {Faker::Internet.free_email}
+    encrypted_password              {Faker::Internet.password(min_length: 6)}
+    encrypted_password_confirmation {password}
+    birthday                        {Faker::Date.birthday}
+    family_name                     {Faker::Japanese::Name.last_name}
+    name                            {Faker::Japanese::Name.first_name}
+    family_name_furigana            {Faker::Japanese::Name.last_name}
+    name_hurigana                   {Faker::Japanese::Name.first_name}
   end
 end
