2d26c695e6d86a1432c223fff4eedb8fef5c534c
Tatsuki-murakami <69897189+Tatsuki-murakami@users.noreply.github.com>
2020-10-01 21:03:28 +0900

    トップページ表示　スタイルシート追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,31 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :family_name, :name, :family_name_furigana, :name_hurigana, :email, 
-             presence: true
 
-  validates :family_name,          presence: true
-  validates :name,                 presence: true
-  validates :family_name_hurigana, presence: true
-  validates :name_hurigana,        presence: true
-  validates :email,                presence: true
-  validates :encrypted_password,   presence: true
-  validates :nickname,             presence: true
-  validates :birthday,             presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-       validates :family_name
-       validates :name
-       validates :family_name_hurigana
-       validates :name_hurigana
+  with_options presence: true do
+    validates :email
+    validates :encrypted_password
+    validates :nickname
+    validates :birthday
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :family_name_hurigana, format: { with: / \ A [ァ-ヶー－] + \ z /, message: '全角カタカナを使用してください' }
+    validates :name_hurigana, format: { with: / \ A [ァ-ヶー－] + \ z /, message: '全角カタカナを使用してください' }
+    validates :email, uniqueness: true
+    validates :encrypted_password, length: { minimum: 6 }
+    validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   end
-
-  validates :email, uniqueness: true
-  validates :encrypted_password, length: { minimum: 6 }
-  validates :encrypted_password,    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-
-  with_options presence: true, format: { with: / \ A [ァ-ヶー－] + \ z /, message: '全角カタカナを使用してください' } do
-    validates :family_name_hurigana
-    validates :name_hurigana
- end
+  
 end
