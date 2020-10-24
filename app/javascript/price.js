74dcd6ee0124ce4613f15829d96c063ede76a496
Tatsuki-murakami <69897189+Tatsuki-murakami@users.noreply.github.com>
2020-10-24 16:52:54 +0900

    js書き換え

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,25 +1,9 @@
-function price(){
-  const priceInput = document.getElementById("item-price");
-  const add_tax = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
-   
-  priceInput.addEventListener('keyup', () => {
-   const value = priceInput.value; 
-         
-    if (value >= 300 && value <= 9999999){
-      let fee = value * 0.1
-      let gains = value - fee; 
-      add_tax.textContent = Math.floor(fee);
-      profit.textContent = Math.floor(gains);
-    } 
-    else {
-      let fee = '-';
-      let gains = '-';
-      add_tax.textContent = fee;
-      profit.textContent = gains;
-    }
+window.addEventListener('load', function () {
+  const item_price = document.getElementById("item-price");
+  const add_tax_price = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');  
+  item_price.addEventListener('change', function() {
+    add_tax_price.textContent = item_price.value * 0.1;
+    profit.textContent = parseInt(item_price.value) + parseInt(add_tax_price.textContent);
   });
-  
-}
-  
-window.addEventListener('load', price);
+});
